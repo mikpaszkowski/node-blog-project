cb9ec433c7638e9c6cdf33e5895dfb2f62ae7ab5
Miki <paszkowski.mikolaj98@gmail.com>
2020-11-13 22:43:59 +0100

    signup-template/password hashing

--- models/dist/user.dev.js
+++ models/dist/user.dev.js
@@ -5,6 +5,8 @@ var mongoose = require('mongoose');
 var _require = require('validator'),
     isEmail = _require.isEmail;
 
+var bcrypt = require('bcrypt');
+
 var userSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -18,5 +20,36 @@ var userSchema = new mongoose.Schema({
     required: true,
     minlength: [10, 'Password legth must be at least 10 characters']
   }
+}); //function which runs after saving to the database
+
+userSchema.post('save', function (doc, next) {
+  console.log('New user was created & saved', doc);
+  next();
+}); //function which runs before saving to the database
+
+userSchema.pre('save', function _callee(next) {
+  var salt;
+  return regeneratorRuntime.async(function _callee$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          _context.next = 2;
+          return regeneratorRuntime.awrap(bcrypt.genSalt());
+
+        case 2:
+          salt = _context.sent;
+          _context.next = 5;
+          return regeneratorRuntime.awrap(bcrypt.hash(this.password, salt));
+
+        case 5:
+          this.password = _context.sent;
+          next();
+
+        case 7:
+        case "end":
+          return _context.stop();
+      }
+    }
+  }, null, this);
 });
 module.exports = mongoose.model('User', userSchema);
